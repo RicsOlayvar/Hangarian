129253fdba91ed048b7ace5a38177a157b7185a9
Rics <202380322@psu.palawan.edu.ph>
2025-08-28 14:07:53 +0800

    k

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Meta:
+     abstract = True
+
+class Priority(BaseModel):
+     name = models.CharField(max_length=150)
+
+     def __str__(self):
+        return self.name
+
+class Category(BaseModel):
+     name = models.CharField(max_length=150)
+
+     def __str__(self):
+        return self.name
+
+class Task(BaseModel):
+     Title = models.CharField(max_length=150)
+     description = models.CharField(max_length=150)
+     deadline = models.DateField()
+     status= models.CharField(max_length=50,choices=[("Pending", "Pending"),("In Progress", "In Progress",),("Completed", "Completed"),] , default="pending")
+     task_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     task_priority = models.ForeignKey(Priority, on_delete=models.CASCADE)
+
+     def __str__(self):
+        return self.title
+
+class Note(BaseModel):
+     related_task = models.ForeignKey(Task, on_delete=models.CASCADE)
+     content = models.TextField()
+
+     def __str__(self):
+        return self.content
+
+class  Subtask (BaseModel):
+     parent_task = models.ForeignKey(Task, on_delete=models.CASCADE)
+     title = models.CharField(max_length=200)
+     status= models.CharField(max_length=50,choices=[("Pending", "Pending"),("In Progress", "In Progress",),("Completed", "Completed"),], default="pending")
+     
+
+     def __str__(self):
+        return self.title
