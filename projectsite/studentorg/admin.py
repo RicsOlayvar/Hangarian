3e873df3eafd675c935fc507e1d1ffcc776de950
Rics <202380322@psu.palawan.edu.ph>
2025-08-28 14:19:06 +0800

    models and admin working properly

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Priority, Category, Task, Note, Subtask
+
+admin.site.register(Priority)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Note)
+admin.site.register(Subtask)
