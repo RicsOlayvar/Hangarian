c8b456a17d9b209787e6b5c9e9c07ce9ca6a7740
Rics <202380322@psu.palawan.edu.ph>
2025-09-17 21:49:55 +0800

    Enhancement Hangarin

--- projectsite/Hangarin/admin.py
+++ projectsite/Hangarin/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import Priority, Category, Task, Note, SubTask
 
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    fk_name = "parent_task"
+    extra = 1
+    fields = ("title", "status")
+    show_change_link = True
+
+class NoteInline(admin.StackedInline):
+    model = Note
+    fk_name = "related_task"
+    extra = 1
+    fields = ("content", "created_at")
+    readonly_fields = ("created_at",)
 
 
 @admin.register(Task)
@@ -9,6 +22,7 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('Title', 'status', 'task_priority', 'task_category', 'deadline')
     list_filter = ('status', 'task_priority', 'task_category')
     search_fields = ('Title', 'description')
+    inlines = [SubTaskInline, NoteInline]
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
