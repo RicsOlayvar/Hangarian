93cf5a44852479ca013f853f778fc8704a6ad637
Rics <202380322@psu.palawan.edu.ph>
2025-10-06 22:07:26 +0800

    tasklist

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -59,7 +59,8 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        #'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
 
         'APP_DIRS': True,
         'OPTIONS': {
