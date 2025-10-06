242be91c6677ef9d57c41c6bc6ef072c027e4b67
Rics <202380322@psu.palawan.edu.ph>
2025-10-06 21:14:28 +0800

    ListView

--- projectsite/Hangarin/views.py
+++ projectsite/Hangarin/views.py
@@ -1,10 +1,40 @@
 from django.shortcuts import render
 
 from django.views.generic.list import ListView
-from Hangarin.models import Priority
+from Hangarin.models import Priority, Category, Task, Note, SubTask 
 
 class HomePageView(ListView):
     model = Priority
     context_object_name = 'home'
     template_name = "home.html"
 
+#LISTVIEW
+class PriorityListView(ListView):
+    model = Priority
+    context_object_name = 'priority'
+    template_name = 'prty_list.html'
+    paginate_by = 5 
+
+class CategoryList(ListView):
+    model = Category
+    context_object_name = 'category'
+    template_name = "catgry_list.html"
+    paginate_by = 5
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'task'
+    template_name = "task_list.html"
+    paginate_by = 5
+
+class NoteList(ListView):
+    model = Note
+    context_object_name = 'note'
+    template_name = "note_list.html"
+    paginate_by = 5
+
+class SubTask(ListView):
+    model = SubTask
+    context_object_name = 'subtask'
+    template_name = "stask_list.html"
+    paginate_by = 5
