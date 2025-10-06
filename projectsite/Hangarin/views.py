5ad947bb7b911ab5413de276573bc10a9f9ab4ef
Rics <202380322@psu.palawan.edu.ph>
2025-10-06 20:43:10 +0800

    modify base

--- projectsite/Hangarin/views.py
+++ projectsite/Hangarin/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic.list import ListView
+from Hangarin.models import Priority
+
+class HomePageView(ListView):
+    model = Priority
+    context_object_name = 'home'
+    template_name = "home.html"
+
